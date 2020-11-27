f0b6b4cd7c2f305f9d0d8319d3447fa8283933c5
NASUNJIN <sj010618@gmail.com>
2020-11-27 21:10:48 +0900

    C_HW

--- 11weeks/printfaddress.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(void) {
-	char str[100] = "Hello, World!";
-	printf("%p\n", str);
-	/*printf((char *)0x008FF934); printf("\n");*/
-	printf("%s\n", str);
-	printf(str);
-}
